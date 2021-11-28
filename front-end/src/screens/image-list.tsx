e7562cc487edbce4c3ab55f2ebc5b9842cd507af
IceCream <icecream@IceCreams-MacBook-Pro.local>
2021-11-28 14:45:32 +0800

    fix image list

--- front-end/src/screens/image-list.tsx
+++ front-end/src/screens/image-list.tsx
@@ -2,15 +2,21 @@ export const ImageList = () => {
   let arr = [
     "https://cdn.myanimelist.net/images/anime/1223/96541.jpg",
     "https://cdn.myanimelist.net/images/anime/1517/100633.jpg",
+    "https://cdn.myanimelist.net/images/anime/5/73199.jpg",
+    "https://cdn.myanimelist.net/images/anime/1337/99013.jpg",
+    "https://cdn.myanimelist.net/images/anime/1122/96435.jpg",
+    "https://cdn.myanimelist.net/images/anime/5/87048.jpg",
+    "https://cdn.myanimelist.net/images/anime/1918/96303.jpg",
+    "https://cdn.myanimelist.net/images/anime/7/81992.jpg",
   ];
 
   return (
-    <div className="h-100-vh w-100-vw flex flex-wrap">
+    <div className="h-100-vh w-100-vw flex flex-wrap justify-center align-center">
       {arr.map((imgLink) => {
         return (
           <img
             src={`${imgLink}`}
-            className="image h-41-vh w-41-vh ma-10"
+            className="image-no-radius h-48-vh w-48-vh"
             style={{ backgroundSize: "contain" }}
           />
         );
